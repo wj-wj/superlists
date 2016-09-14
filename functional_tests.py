477fb1066d6a4c3a38b3b0c4fa5be6cd44da2178
wangjie <wngjie5633@163.com>
2016-09-14 17:14:32 +0800

    Basic view now reutrns minimal HTML.

--- functional_tests.py
+++ functional_tests.py
@@ -3,14 +3,14 @@
 
 
 from selenium import webdriver
-import unittest
+import unittest, warnings
 
 
 class NewVisitiorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait()
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -19,8 +19,9 @@ class NewVisitiorTest(unittest.TestCase):
 
 		self.browser.get('httP://localhost:8000')
 
-		self.assertIn 'To-Do' in browser.title
+		self.assertIn('To-Do', self.browser.title)
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+	with warnings.catch_warnings(record=True):
+		unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -1,17 +1,29 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
 
-#class SmokeTest(TestCase):
-	
-#	def test_bad_maths(self):
-#		self.assertEqual(1 + 1, 3)
+
+# class SmokeTest(TestCase):
+# 	def test_bad_maths(self):
+# 		self.assertEqual(1 + 1, 3)
 	
 	
 class HomePageTest(TestCase):
 	
 	def test_root_url_resolvers_to_home_page_view(self):
-		found =resolve('/')
+		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_corrent_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
+
+	def test_git(self):
+		pass
