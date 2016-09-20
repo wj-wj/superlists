2aa7cf11d1c42a5a6a4ecb1ac6536d6888a7ce25
wj-wj <wangjie5633@163.com>
2016-09-21 01:31:01 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,9 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from lists.models import Item
 from lists.views import home_page
+
 
 # Create your tests here.
 
@@ -11,33 +13,54 @@ from lists.views import home_page
 # class SmokeTest(TestCase):
 # 	def test_bad_maths(self):
 # 		self.assertEqual(1 + 1, 3)
-	
-	
+
+
 class HomePageTest(TestCase):
-	
-	def test_root_url_resolvers_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+    def test_root_url_resolvers_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
-	def test_home_page_returns_corrent_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		expected_html = render_to_string('home.html')
-		self.assertEqual(response.content.decode(), expected_html)
-		# self.assertTrue(response.content.startswith(b'<html>'))
-		# self.assertIn(b'<title>To-Do lists</title>', response.content)
-		# self.assertTrue(response.content.strip().endswith(b'</html>'))
+    def test_home_page_returns_corrent_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
 
-	def test_home_page_can_save_a_POST_request(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
+    # self.assertTrue(response.content.startswith(b'<html>'))
+    # self.assertIn(b'<title>To-Do lists</title>', response.content)
+    # self.assertTrue(response.content.strip().endswith(b'</html>'))
 
-		response = home_page(request)
-		self.assertIn('A new list item', response.content.decode())
-		expected_html = render_to_string(
-			'home.html',
-			{'new_item_text': 'A new list item'}
-		)
-		self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
 
